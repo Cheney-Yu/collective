099fd7934c2a6a8a13ad4bb6e62c07231d2418ed
Cheney-Yu <67189643+Cheney-Yu@users.noreply.github.com>
2020-08-19 10:13:35 +0800

    Create collectiove.sh

---
+++ collectiove.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#
+# Auto install collective Scripts (diverse usage)
+#
+# Copyright (C) 2019-2020 Cheney-Yu <benkabu8875@gmail.com>
+#
+# System Required:  CentOS 6+, Debian7+, Ubuntu12+
+#
+# Reference URL:
+# ------------------Proxy Script--------------------------
+# Shadowsocks Installation:(Copyright from teddysun)
+# https://github.com/teddysun/shadowsocks_install/tree/master
+# V2ray Installation:
+#
+# Trojan Installation:
+# 
+# Socks5 Proxy Installation(over tls):
+#
+# ------------------Transit Script------------------------
+# gost Installation:
+#
+# ---------------Useful Tools Installation----------------
+# gost Installation:
+#
+# ufw Installation:
+#
+# ------------------System Script-------------------------
+# bbr activate:
+# --------------------------------------------------------
+#
+# Thanks:
+# @clowwindy  <https://twitter.com/clowwindy>
+# @breakwa11  <https://twitter.com/breakwa11>
+# @cyfdecyf   <https://twitter.com/cyfdecyf>
+# @madeye     <https://github.com/madeye>
+# @linusyang  <https://github.com/linusyang>
+# @Akkariiin  <https://github.com/Akkariiin>
+# 
+# Intro:  https://teddysun.com/486.html
